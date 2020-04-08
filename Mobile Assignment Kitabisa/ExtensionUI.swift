4224ef85446de3a237d7879f76819d3264a0c2b2
Clszz <mreynaldi908@gmail.com>
2020-04-08 13:52:35 +0700

    Add baseproject

---
+++ Mobile Assignment Kitabisa/ExtensionUI.swift
@@ -0,0 +1,47 @@
+//
+//  ExtensionUI.swift
+//  Mobile Assignment Kitabisa
+//
+//  Created by Muhammad Reynaldi on 08/04/20.
+//  Copyright © 2020 -. All rights reserved.
+//
+
+import UIKit
+
+extension UIView{
+    func setRounded() {
+        self.layer.masksToBounds = false
+        self.layer.cornerRadius = self.frame.height/2
+        self.clipsToBounds = true
+    }
+    
+    func outerRound() {
+        self.layer.cornerRadius = 5
+        self.clipsToBounds = true
+    }
+    
+}
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIView.ContentMode) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: UIView.ContentMode) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+    
+}
+
+

f74313bbee3a964820c151d4607976d20eab5a7e
Clszz <mreynaldi908@gmail.com>
2020-04-08 13:42:13 +0700

    Initial Commit

---
+++ Mobile Assignment Kitabisa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobile Assignment Kitabisa
+//
+//  Created by Muhammad Reynaldi on 08/04/20.
+//  Copyright © 2020 -. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

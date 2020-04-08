fc7d7cd12bc3a11f07076799b7b641d64a89da19
Clszz <mreynaldi908@gmail.com>
2020-04-08 14:06:24 +0700

    Add cardManuView and Protocols

---
+++ Mobile Assignment Kitabisa/ListMovieProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ListMovieProtocol.swift
+//  Mobile Assignment Kitabisa
+//
+//  Created by Muhammad Reynaldi on 08/04/20.
+//  Copyright © 2020 -. All rights reserved.
+//
+
+import Foundation
+
+protocol CategoryProtocol {
+    func categoryTapped(category:String)
+}
